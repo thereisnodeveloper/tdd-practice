d960ea77b56f841c2c3b0ecda1642dd9591ed57b
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-10-29 09:59:40 -0400

    feat: add capitalize()

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 
 
-export default function capitalize() {
-  
+/**
+ * Capitalizes the first letter of the given string.
+ *
+ * @param {string} string - The input string to be capitalized.
+ * @returns {string} The input string with its first letter capitalized.
+ */
+export default function capitalize(string) {
+  return string.charAt(0).toUpperCase().concat(string.substring(1))
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,27 @@
-/** @jest-environment jsdom */
+/**DISABLED jest-environment jsdom */
 
-import capitalize from "./index.js";
+import capitalize from './index.js';
 
 describe('capitalize', () => {
+  // takes a string and returns it with the first character capitalized
   it('exists', () => {
     expect(capitalize).toBeDefined();
   });
 
+  it('returns', () => {
+    expect(capitalize('hello')).toBeDefined();
+  });
+  
+    // it('returns 1st char capitalized', () => {
+    //   expect(capitalize('goodbye')).toBe('G')
+    // });
+  it('returns string with 1st char capitalized', () => {
+    expect(capitalize('hello')).toBe('Hello')
+  });
+  it('returns string with 1st char capitalized', () => {
+    expect(capitalize('goodbye')).toBe('Goodbye')
+  });
+  // it('returns substring after 1st char',()=>{
+  //   expect(capitalize('hello')).toBe('ello')
+  // })
 });
-
