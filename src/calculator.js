3ef675dd2ab31329c1a427c507dcc5fce562d243
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-10-29 11:28:47 -0400

    add subtract() to calculator

--- src/calculator.js
+++ src/calculator.js
@@ -2,4 +2,7 @@ export const calculator = {
   add(number1, number2) {
     return number1 + number2;
   },
+  subtract(number1, number2){
+    return number1-number2
+  }
 };
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -8,20 +8,32 @@ describe('calculator', () => {
     expect(calculator).toBeDefined();
   });
 
-  it('is an object',()=>{
-    expect(calculator).toBeInstanceOf(Object)
-  })
+  it('is an object', () => {
+    expect(calculator).toBeInstanceOf(Object);
+  });
 
-  it('contains add()', ()=>{
-    expect(calculator).toHaveProperty('add')
-  })
+  it('contains add()', () => {
+    expect(calculator).toHaveProperty('add');
+  });
+  it('contains subtract()', () => {
+    expect(calculator).toHaveProperty('subtract');
+  });
 });
 
-describe('add',()=>{
-    it('adds 2 numbers',()=>{
-        expect(calculator.add(1,2)).toBe(3)
-    }),
-    it('adds 2 numbers',()=>{
-        expect(calculator.add(10,5)).toBe(15)
-    })
-})
+describe('add', () => {
+  it('adds 2 numbers', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+  }),
+  it('adds 2 numbers', () => {
+    expect(calculator.add(10, 5)).toBe(15);
+  });
+});
+
+describe('subtract', () => {
+  it('subtracts num2 from num1', () => {
+    expect(calculator.subtract(5, 1)).toBe(4);
+  });
+  it('subtracts num2 from num1', () => {
+    expect(calculator.subtract(16, 4)).toBe(12);
+  });
+});
